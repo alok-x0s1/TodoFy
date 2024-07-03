03876aba46987ecbcfcb26d5726dded822644fc0
Alok Yadav <yalok6321@gmail.com>
2024-07-03 21:56:15 +0530

    signup controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,46 @@
+import User from "../models/user.model.js";
+import generateToken from "../utils/generateToken.js";
+import { generateHashedPassword } from "../utils/hashPassword.js";
+import { signupSchema } from "../utils/zodValidation.js";
 
 const signupUser = async (req, res) => {
-    const {username, email, password} = req.body;
+    try {
+        const {username, email, password} = req.body;
+        const isValid = signupSchema.safeParse({username, email, password})
+        if (!isValid.success) {
+            return res.send("Zod :: Data validation error :: ", isValid.error)
+        }
+    
+        const existedUser = await User.findOne({
+            $or: [{ username }, { email }]
+        })
+    
+        if(existedUser) {
+            return res.status(400).send("User with email or password already exist.")
+        }
+    
+        const hashedPassword = await generateHashedPassword(password)
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+    
+        const createdUser = await User.findById(user._id)
+        if(!createdUser) {
+            return res.status(500).send("Internal server error :: Error while creating the user")
+        }
+    
+        const token = generateToken({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        })
+    
+        res.cookie("token", token).redirect("/")
+    } catch (error) {
+        return res.status(500).send("Server error. Please try again later.");
+    }
 }
 
 const loginUser = async (req, res) => {
