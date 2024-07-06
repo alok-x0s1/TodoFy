4d0e1e846aa16b3f245be89c9f2ee03f34e19139
Alok Yadav <yalok6321@gmail.com>
2024-07-06 09:49:55 +0530

    define routes for controller

--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import isLoggedIn from "../middlewares/auth.middleware.js";
-import { createTodo, deleteTodo } from "../controllers/todo.controller.js";
+import { createTodo, deleteTodo, getAllTodos } from "../controllers/todo.controller.js";
 
 const router = express.Router();
 
 router.route("/create-todo").post(isLoggedIn, createTodo)
 router.route("/delete-todo/:id").delete(isLoggedIn, deleteTodo)
+router.route("/all").get(isLoggedIn, getAllTodos)
 
 export default router;
