970f7a285adbf327da5e275f76e7cc73a8d8d069
Alok Yadav <yalok6321@gmail.com>
2024-07-24 15:27:36 +0530

    cors config

--- backend/config/index.js
+++ backend/config/index.js
@@ -7,7 +7,11 @@ import cors from "cors";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+	cors({
+		credentials: true,
+	})
+);
 
 // Imports
 import userRouter from "../routes/user.route.js";
