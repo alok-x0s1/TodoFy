aa9ff35fa802e05117190078c299c9e8c413ddfb
Alok Yadav <yalok6321@gmail.com>
2024-08-23 10:22:43 +0530

    Main page to export multiple pages

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,9 @@
+import CreateTodo from "./CreateTodo";
+import EditTodo from "./EditTodo";
+import Home from "./Home";
+import Login from "./Login";
+import Profile from "./Profile";
+import Signup from "./Signup";
+import SingleTodo from "./SingleTodo";
+
+export { Login, Home, Signup, Profile, CreateTodo, SingleTodo, EditTodo };
